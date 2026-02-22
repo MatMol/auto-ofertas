24db6ebc1665f6849df06bda82d03f204c691688
Mateo Molina <mmolina@gofundme.com>
2026-02-22 18:43:17 -0300

    fix: images again

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,36 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "http2.mlstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.mlstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.prd.kavak.io",
-      },
-      {
-        protocol: "https",
-        hostname: "*.kavak.io",
-      },
-      {
-        protocol: "https",
-        hostname: "acroadtrip.blob.core.windows.net",
-      },
-      {
-        protocol: "https",
-        hostname: "acnews.blob.core.windows.net",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
